bfa7a769d19b0bad1d67a95a45b438366e4f149c
Sam McDonald <50050303'belfastmet.ac.uk>
2024-10-17 14:18:45 +0100

    Initial commit

---
+++ practice.py
@@ -0,0 +1,10 @@
+my_string ="this is a string"
+print(my_string)
+print(my_string[6])
+print(my_string[1:3])
+print(my_string[-1])
+
+my_name = input("please enter your name")
+print("your name is " +my_name)
+my_age = input("please enter your age:")
+print ('your age is ' + my_age + '!!!!')
